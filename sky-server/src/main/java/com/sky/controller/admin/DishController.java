cea9e5fcfc6b5b2ec7e20b7cc55b7fb5784d4e82
Honggeer <honggeer1208@gmail.com>
2024-09-03 13:53:18 -0400

    edit the dish info

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -5,6 +5,7 @@ import com.sky.dto.DishPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -43,6 +44,20 @@ public class DishController {
     public Result delete(@RequestParam List<Long> ids){
         log.info("dishes batch deletion: {}",ids);
         dishService.deleteBatch(ids);
+        return Result.success();
+    }
+    @GetMapping("/{id}")
+    @ApiOperation("search dishes by id")
+    public Result<DishVO> getById(@PathVariable Long id){
+        log.info("Search dishes by id: {}",id);
+        DishVO dishVO=dishService.getByIdWithFlavor(id);
+        return Result.success(dishVO);
+    }
+    @PutMapping
+    @ApiOperation("edit dish info")
+    public Result update(@RequestBody DishDTO dishDTO){
+        log.info("Edit dish info: {}",dishDTO);
+        dishService.updateWithFlavor(dishDTO);
         return Result.success();
     }
 
